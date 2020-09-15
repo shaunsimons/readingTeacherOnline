fbb63b926ab1ad9a71ac4f0747cb6f447b1469a3
Shaun <shaunsimons93@gmail.com>
2020-09-15 12:08:50 +1000

    allowed host

--- readingTeacherOnline/settings.py
+++ readingTeacherOnline/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shaun1330.pythonanywhere.com']
 
 
 # Application definition
