e6aeaa75e2d6ced9371e603e00d5a0967ad421c6
Shaun <shaunsimons93@gmail.com>
2020-10-06 10:20:12 +1100

    all courses page links

--- courses_site/models.py
+++ courses_site/models.py
@@ -14,7 +14,6 @@ class Course(models.Model):
 
     def get_absolute_url(self):
         kwargs = {
-            'pk': self.id,
             'slug': self.slug
         }
         return reverse('course_detail', kwargs=kwargs)
