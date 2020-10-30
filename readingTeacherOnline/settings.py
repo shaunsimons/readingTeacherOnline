b99ed2ee61b7459eaa03941eb24b750cd166f63f
Shaun Simons <shaunsimons93@gmail.com>
2020-10-30 17:32:02 +1100

    lightgallery plugin

--- readingTeacherOnline/settings.py
+++ readingTeacherOnline/settings.py
@@ -128,8 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'node_modules',
+]
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
