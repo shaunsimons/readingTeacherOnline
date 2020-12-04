3393f54b1dab6ea725a5b62051556ae87c5d21d4
Shaun Simons <shaunsimons93@gmail.com>
2020-12-04 14:52:11 +1100

    mysql db

--- readingTeacherOnline/settings.py
+++ readingTeacherOnline/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'readingTeacherOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_USERNAME$DBNAME'),
+        'USER': os.environ.get('MYSQL_USERNAME'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOSTNAME'),
     }
 }
 
